b12550b0f16bdafedf25f709773aa1bef69d6036
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-12-04 18:27:32 -0700

    Oh dear

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,51 +1,49 @@
 class BinarySearchTree
-  attr_accessor :root
+  attr_reader :root
 
   def initialize
     @root = nil
   end
 
   def insert(score, title)
-    @root = Node.new(score, title)
-    current = nil
+    new_node = Node.new(score, title)
     if @root.nil?
-      current = @root
+       @root = new_node
     else
-      post_insert(current, root)
-    # elsif current.score > root.score
-    #   current.right_insert(score, title)
-    # elsif current.score < root.score
-    #   current.left_insert(score, title)
+      post_insert(new_node, root_node)
     end
   end
 
-  def post_insert(current, root)
-    if current.score > root.score
+  # require "pry"; binding.pry
+  def post_insert(new_node, root_node)
+    # 4 conditions here
+    if new_node.score > root.score && root.right_next_node != nil
+      # if greater than, and not nil move right
+      # post_insert(new_node, root.right)
        right_insert(score, title)
-
-
-    else
-      current.score < root.score
+    elsif new_node.score > root.score && root.right_next_node.nil?
+         new_node = root_node.right_next_node
+      # elsif greater than and nil, insert
+    elsif new_node.score < root.score && root.right_next_node != nil
       left_insert(score, title)
+      # elsif less than and not nil move left
+    elsif new_node.score < root.score && new_node.nil?
+      right_insert(score, title)
+      # else less than and nil, insert
+    end
   end
-  #
-  def right_insert(score, title#, current = @root)
-    depth = 0
-  #     if root.score < current.score
+
+  def right_insert(score, title)
+    if root.score < current.score
+        post_insert.current = root.right_next_node
+        depth += 1
+    end
+  end
+
+  def left_insert(score, title, current = @root)
+    if root.score < current.score
         current = root.right_next_node
         depth += 1
-  #     else
-  #       depth = 0
-  #     end
-  # end
-  #
-  # def left_insert(score, title, current = @root)
-  #   depth = 0
-  #     if root.score < current.score
-  #       current = root.right_next_node
-  #       depth += 1
-  #     else
-  #       depth = 0
-  #     end
-  # end
+    end
+  end
 end
